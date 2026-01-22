fb154623d54504f6dd8bbaefa34b6b4fb6639ccb
Umang Soni <soni.umang921@gmail.com>
2026-01-22 11:27:13 +0530

    Fix the login and signup

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.route("/signup").get(userController.renderSignup)
   wrapAsync(userController.signup)
 )
 
-router.route("/login").get( userController.renderLoginForm)
+router.route("/login").get(userController.renderLoginForm)
 .post(
   saveRedirectUrl,
   passport.authenticate("local", {
