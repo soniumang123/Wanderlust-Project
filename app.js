0e11afaa8ff5b4ae9caa5e979eb33727c180c585
Umang Soni <soni.umang921@gmail.com>
2026-02-03 09:05:11 +0530

    Fix flash issue

--- app.js
+++ app.js
@@ -79,14 +79,13 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.use((req,res,next)=>{
-  console.log("REQ.USER =>", req.user);
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currentUser = req.user;
   next();
 });
+
 
 app.use((req,res,next)=>{
   console.log("SESSION =>",req.session);
